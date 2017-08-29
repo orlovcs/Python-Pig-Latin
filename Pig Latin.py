09e964c8a94f4a96a9a30cb77822eea1041525d5
thetax45 <thetax45@gmail.com>
2017-08-28 23:43:39 -0400

    Update Pig Latin.py

--- Pig Latin.py
+++ Pig Latin.py
@@ -2,13 +2,12 @@ def pythonpiglatin():
     pyg = 'ay'
     original = input('Enter a word:')
     if len(original) > 0 and original.isalpha():
-        print(original)
+        print('Original word is: ' + original)
         word=original.lower()
         first=word[0]
         new_word=word+first+pyg
-        print(len(new_word))
         new_word=new_word[1:7]
-        print(new_word)
+        print ("Length of new word is: " + str(len(new_word)))
+        print('Word in pig latin is: ' + new_word)
     else:
-        print("empty")
-    print(first)
+        print("Word is invalid")
